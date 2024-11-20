9af5e6b4a324d1177d2bbe74bd8f0abeae0b7e6b
Hugo <hugo.lacourr@gmail.com>
2024-11-20 20:19:28 +0100

    Initial Commit

---
+++ TrackRuns/TrackRunsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackRunsApp.swift
+//  TrackRuns
+//
+//  Created by Lacour Hugo on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackRunsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackRunsTests/TrackRunsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackRunsTests.swift
+//  TrackRunsTests
+//
+//  Created by Lacour Hugo on 20/11/2024.
+//
+
+import Testing
+@testable import TrackRuns
+
+struct TrackRunsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
